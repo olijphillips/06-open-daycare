caaa4a2d827733bb1b28557ee5477ff1ffda2599
olijphillips <olijphillips@hotmail.com>
2026-08-17 19:32:49 -0600

    feat: Integrate Supabase client and middleware for session management

---
+++ utils/supabase/server.ts
@@ -0,0 +1,28 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+export const createClient = (cookieStore: Awaited<ReturnType<typeof cookies>>) => {
+  return createServerClient(
+    supabaseUrl!,
+    supabaseKey!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+          } catch {
+            // El método `setAll` fue llamado desde un Server Component.
+            // Esto se puede ignorar si hay un proxy/middleware refrescando
+            // las sesiones del usuario.
+          }
+        },
+      },
+    },
+  );
+};
